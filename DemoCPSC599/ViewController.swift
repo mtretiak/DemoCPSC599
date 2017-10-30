56e54afe3b019a3f1b503112e39e956f6160956d
Victoria Lappas <victorialappas@Victorias-MacBook-Pro.local>
2017-10-30 15:38:53 -0600

    Initial Commit

---
+++ DemoCPSC599/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoCPSC599
+//
+//  Created by Victoria Lappas on 2017-10-30.
+//  Copyright © 2017 Victoria Lappas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
